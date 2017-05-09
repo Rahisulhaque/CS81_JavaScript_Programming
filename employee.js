4e0ef45574603c212b4a8f20f7d5b444264c2922
Rahisul Haque <rhaque@localhost.localdomain>
2017-05-09 14:30:03 -0700

    Assignmnet 05, 06

--- employee.js
+++ employee.js
@@ -1,1 +1,16 @@
-var 
+var employee(firstname, lastname, ssn, eid,dpt)
+	{
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this. ssn = ssn;
+		this.eid = eid;
+		this.dpt = dpt;
+	};
+employee.prototype.method = funtion()
+{
+
+	console.log(this.first+''+this.last+ ":"+ this.dpt);
+};
+
+var eJohnDoe = new employee("John", "Doe", "123-34-1234", "001234", "acct");
+var eSallyRally = new employee("Sally", "Rally","123-34-1235", "001235", "hr");
